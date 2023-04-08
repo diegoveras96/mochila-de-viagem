0e29b88a4c4f951cbe5ed9ef34b9b3032167ef71
Diego Veras <diego.veras96@hotmail.com>
2023-04-08 19:20:13 -0300

    Projeto Finalizado

--- js/main.js
+++ js/main.js
@@ -2,32 +2,36 @@ const form = document.querySelector('#novoItem');
 const lista = document.querySelector('#lista');
 const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
-itens.forEach((elemento)=>{
+
+itens.forEach((elemento) => {
     createNewElement(elemento);
 })
 
-form.addEventListener("submit", (evento)=>{
+form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
     const exist = itens.find(elemento => elemento.nome === nome.value);
+    if (nome.value == "" || quantidade.value == "") {
+        alert("Dado Faltando\nInsira os Dados Novamente")
+    } else {
+        const atualItem = {
+            "nome": nome.value,
+            "quantidade": quantidade.value
+        };
 
-    const atualItem ={
-        "nome": nome.value,
-        "quantidade": quantidade.value
-    };
+        if (exist) {
+            atualItem.id = exist.id;
+            updateElement(atualItem);
 
-    if(exist){
-        atualItem.id = exist.id;
-        updateElement(atualItem);
+            itens[itens.findIndex(elemento => elemento.id === exist.id)] = atualItem;
+        } else {
+            atualItem.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
+            createNewElement(atualItem);
 
-        itens[exist.id] = atualItem;
-    } else{
-        atualItem.id = itens.length;
-        createNewElement(atualItem);
-
-        itens.push(atualItem);
+            itens.push(atualItem);
+        }
     }
 
     localStorage.setItem("itens", JSON.stringify(itens));
@@ -37,8 +41,8 @@ form.addEventListener("submit", (evento)=>{
 
 })
 
-function createNewElement(item){
-    
+function createNewElement(item) {
+
     const newItem = document.createElement('li');
     newItem.classList.add('item');
 
@@ -49,22 +53,25 @@ function createNewElement(item){
     newItem.appendChild(itemNumber);
     newItem.innerHTML += item.nome;
 
-    newItem.appendChild(deleteButton());
+    newItem.appendChild(deleteButton(item.id));
     lista.appendChild(newItem);
 
 }
 
-function updateElement(item){
-    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+function updateElement(item) {
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
 }
 
-function deleteButton(){
+function deleteButton(id) {
     const buttonElement = document.createElement("button");
     buttonElement.innerText = "X";
 
-    buttonElement.addEventListener("click", function(){
+    buttonElement.addEventListener("click", function () {
         this.parentNode.remove();
-    })
 
+        itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+        localStorage.setItem("itens", JSON.stringify(itens));
+    })
     return buttonElement
 }
